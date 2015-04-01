2bf85ec7d72243fa50b436d43e7d37293997b6f8
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 17:34:16 -0400

    fixed redirect testing for all post controller routes

--- spec/controller/crud_spec.rb
+++ spec/controller/crud_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../spec_helper'
 
 describe 'posts controller' do
-  it 'should process the GET route and redirect to the /posts erb page' do
+  it 'should process the GET route and make it redirect to the post index erb page' do
     get '/posts'
     expect(last_response).to be_redirect
     follow_redirect!
@@ -9,23 +9,32 @@ describe 'posts controller' do
     expect(last_response.body).to include('Welcome to AirBNL!')
   end
 
-  it 'should process the post /posts route' do
-    post '/posts'
+  it 'should process the post route and make it redirect to the post index erb page' do
+    post '/posts/:id'
     expect(last_response).to be_redirect
     follow_redirect!
     expect(last_response).to be_ok
-    expect(last_response.body).to include('The best')
+    expect(last_response.body).to include('Welcome to AirBNL!')
     end
 
 
 
-    it 'should process the delete /posts/:id/delete route' do
+    it 'should process the delete route and make it redirect to the post index erb page' do
       # delete '/delete_something', :params => with_a_param=true
-      delete '/delete_something'
+      delete '/posts/:id/delete'
       expect(last_response).to be_redirect
       follow_redirect!
       expect(last_response).to be_ok
-      expect(last_response.body).to include('The best')
+      expect(last_response.body).to include('Welcome to AirBNL!')
+    end
+
+    it 'should process the edit route and make it redirect to the post show erb page' do
+      # delete '/delete_something', :params => with_a_param=true
+      put '/posts/:id/edit'
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('Created by')
     end
 
 end
