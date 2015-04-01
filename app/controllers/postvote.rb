5fe34951c1b4384bbf293d3215ae551fd75de55d
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 15:50:59 -0400

    added ajax skeleton

--- app/controllers/postvote.rb
+++ app/controllers/postvote.rb
@@ -2,8 +2,8 @@ post '/posts/:id/upvote' do
   @the_post = Post.find_by(:id => params[:id])
   if User.find_by(id: session[:user_id])
     @upvote = Postvote.create(:post_id => @the_post.id, :upvote => 1, :downvote => 0 ,:user_id => session[:user_id])
-    # erb :"_postvote", :layout => false
-    redirect "/posts/#{@the_post.id}"
+    erb :"_postvote", :layout => false
+    # redirect "/posts/#{@the_post.id}"
   else
     "Sorry, only AirBnl members can vote!"
   end
@@ -14,8 +14,8 @@ post '/posts/:id/downvote' do
   @the_post = Post.find_by(:id => params[:id])
   if User.find(session[:user_id])
     @downvote = Postvote.create(:post_id => @the_post.id, :upvote => 0, :downvote => 1, :user_id => session[:user_id])
-    # erb :"_postvote", :layout => false
-    redirect "/posts/#{@the_post.id}"
+    erb :"_postvote", :layout => false
+    # redirect "/posts/#{@the_post.id}"
   else
     "Sorry, only AirBnl members can vote!"
   end
