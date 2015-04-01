5c1b016137f499d1e404c10702f4e386a0fd0164
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 16:46:26 -0400

    added ajax for create comment

--- public/js/application.js
+++ public/js/application.js
@@ -9,4 +9,17 @@ $('.pvotes').on('submit', function(event){
     $('#votes').replaceWith(response)
   });
 });
+
+$('#commentlink').on('click', function(event){
+  console.log(event)
+  event.preventDefault();
+  $.ajax({
+    type: "GET",
+    url: this.href,
+  }).done(function(response){
+    console.log(response)
+    $('#comments_section').append(response)
+  });
+});
+
 });
