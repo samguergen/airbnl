bd37b3d8ca85444d462593ec7a7194253f6c505a
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 10:50:52 -0400

    filled in most of post route with instance variable class parsing and redirects

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,28 +1,38 @@
 get '/posts' do
-
+  @all_posts = Post.all
+  erb :"post/index"
 end
 
 get '/posts/new' do
-
+  erb :"post/new"
 end
 
 get '/posts/:id' do
-
+  @the_post = Post.find_by(id: params[:id])
+  erb :"post/show"
 end
 
 get '/posts/:id/edit' do
-
+  @the_post = Post.find_by(id: params[:id])
+  erb :"/post/edit"
 end
 
 put '/posts/:id' do
-
+  @the_post = Post.find_by(id: params[:id])
 end
 
 post '/posts' do
-
+  @new_post = Post.new()
+  if @new_post.save!
+    redirect "/posts/#{@new_post.id}"
+  else
+    [404, "This post couldn't be added."]
+  end
 end
 
 
 delete '/posts/:id/delete' do
-
+  @post_to_delete = Post.find_by(id: params[:id])
+  @post_to_delete.destroy!
+  redirect "/posts"
 end
