3b632b993619fd284b5c7934a004f44a0014cef0
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 15:23:13 -0400

    added postvote constraints to controller

---
+++ app/controllers/postvote.rb
@@ -0,0 +1,20 @@
+post '/posts/:id/upvote' do
+  @the_post = Post.find_by(:id => params[:id])
+  if User.find(session[:user_id])
+    @upvote = Postvote.create(:post_id => @the_post.id, :upvote => 1, :downvote => 0 ,:user_id => session[:user_id])
+    erb :"_postvote", :layout => false
+  else
+    "Sorry, only AirBnl members can vote!"
+  end
+end
+
+
+post '/posts/:id/downvote' do
+  @the_post = Post.find_by(:id => params[:id])
+  if User.find(session[:user_id])
+    @downvote = Postvote.create(:post_id => @the_post.id, :upvote => 0, :downvote => 1, :user_id => session[:user_id])
+    erb :"_postvote", :layout => false
+  else
+    "Sorry, only AirBnl members can vote!"
+  end
+end
