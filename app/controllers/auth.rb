982d0cbc70e84fde921c78fd9f6fe117b93fd7e9
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 14:17:32 -0400

    added signup route, signup form and logout route

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,18 +3,30 @@ get '/login' do
 end
 
 post '/login' do
-
+  user = User.find_by(email: params[:user][:email])
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+  else
+    redirect "/login"
+  end
 end
 
 get '/signup' do
-
+  erb :"auth/signup"
 end
 
 post '/signup' do
-
+  new_user = User.new(params[:user])
+  if new_user.save!
+    session[:user_id] = new_user.id
+    redirect "/posts"
+  else
+    redirect "/signup"
+  end
 end
 
 get '/logout' do
-
+  session[:user_id] = nil
+  redirect "/posts"
 end
 
