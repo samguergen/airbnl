e1ca17e9836fdc1b60da5e549c2726d8b6b89ba0
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 10:36:21 -0400

    added index route and post route skeleton

---
+++ app/controllers/post.rb
@@ -0,0 +1,28 @@
+get '/posts' do
+
+end
+
+get '/posts/new' do
+
+end
+
+get '/posts/:id' do
+
+end
+
+get '/posts/:id/edit' do
+
+end
+
+put '/posts/:id' do
+
+end
+
+post '/posts' do
+
+end
+
+
+delete '/posts/:id/delete' do
+
+end
