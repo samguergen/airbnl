25e21c914c382886a8356be2278d8b906297f2ac
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 15:39:00 -0400

    fixed upvote by changing find to find_by in constraints

--- app/controllers/postvote.rb
+++ app/controllers/postvote.rb
@@ -1,8 +1,9 @@
 post '/posts/:id/upvote' do
   @the_post = Post.find_by(:id => params[:id])
-  if User.find(session[:user_id])
+  if User.find_by(id: session[:user_id])
     @upvote = Postvote.create(:post_id => @the_post.id, :upvote => 1, :downvote => 0 ,:user_id => session[:user_id])
-    erb :"_postvote", :layout => false
+    # erb :"_postvote", :layout => false
+    redirect "/posts/#{@the_post.id}"
   else
     "Sorry, only AirBnl members can vote!"
   end
@@ -13,7 +14,8 @@ post '/posts/:id/downvote' do
   @the_post = Post.find_by(:id => params[:id])
   if User.find(session[:user_id])
     @downvote = Postvote.create(:post_id => @the_post.id, :upvote => 0, :downvote => 1, :user_id => session[:user_id])
-    erb :"_postvote", :layout => false
+    # erb :"_postvote", :layout => false
+    redirect "/posts/#{@the_post.id}"
   else
     "Sorry, only AirBnl members can vote!"
   end
