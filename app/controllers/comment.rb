59c7a56715d87bd29e75374a9d1f071b46a770d7
Samantha Guergenenov <samguergen@gmail.com>
2015-04-01 10:39:20 -0400

    added comment route skeleton

---
+++ app/controllers/comment.rb
@@ -0,0 +1,25 @@
+
+get '/posts/:id/comments' do
+
+end
+
+get '/posts/:id/comments/new' do
+
+end
+
+get '/posts/:id/comments/:commentid/edit' do
+
+end
+
+put '/posts/:id/comments/:commentid' do
+
+end
+
+post '/posts/:id/comments' do
+
+end
+
+
+delete '/posts/:id/comments/:commentid/delete' do
+
+end
